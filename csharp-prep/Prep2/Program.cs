f25a21ead6aee077c476452ffdbe84644af6ca0f
levijohnson1227 <lw.johnson.1227@gmail.com>
2023-09-16 13:44:41 -0600

    code written

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What was your grade percentage? ");
+        string grade = Console.ReadLine();
+        int number = int.Parse(grade);
+
+        string lettergrade = "";
+
+        if (number >= 90)
+        {
+            lettergrade = "A";
+        }
+        else if (number >= 80)
+        {
+            lettergrade = "B";
+        }
+        else if (number >= 70)
+        {
+            lettergrade = "C";
+        }
+        else if (number >= 60)
+        {
+            lettergrade = "D";
+        }
+        else
+        {
+            lettergrade = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is {lettergrade}");
+
+        if (number >= 70)
+        {
+            Console.WriteLine("Awesome, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Sadly you failed.");
+        }
     }
 }
