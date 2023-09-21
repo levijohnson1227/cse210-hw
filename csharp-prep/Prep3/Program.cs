35ed13f19b93346df4e6f51b80984250c8e5eaa2
levijohnson1227 <lw.johnson.1227@gmail.com>
2023-09-20 18:45:29 -0600

    first step

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("Please provide a random number: ");
+        string magic = Console.ReadLine();
+        int number = int.Parse(magic);
+
+        Console.Write("What is your guess? ");
+        string guess = Console.ReadLine();
+        int guessednumber = int.Parse(guess);
+
+        if (guessednumber == number)
+        {
+            Console.WriteLine("You guessed it!");
+        }
+        else if (guessednumber < number)
+        {
+            Console.WriteLine("Higher");
+        }
+        else
+        {
+            Console.WriteLine("Lower");
+        }
+
     }
 }
