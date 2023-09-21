5a93d6e6753f1ff9da383547ee25f2698a1ffe52
levijohnson1227 <lw.johnson.1227@gmail.com>
2023-09-20 21:07:23 -0600

    finished

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -6,7 +6,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        List<int> numbers = new List<int>();
+        List<int> usernumbers = new List<int>();
         
         Console.WriteLine("Enter a list of numbers, type 0 when finished.");
 
@@ -19,9 +19,33 @@ class Program
 
            if (number != 0)
            {
-            numbers.Add(number);
+            usernumbers.Add(number);
            } 
         } 
-        Console.WriteLine(numbers);
+        
+        int sum = 0;
+        foreach (int usernumber in usernumbers)
+        {
+            sum += usernumber;
+        }
+
+        Console.WriteLine($"The sum of the numbers is: {sum}");
+
+        double average = ((double)sum) / usernumbers.Count;
+        Console.WriteLine($"The average of the numbers is: {average}");
+
+        int largest = usernumbers[0];
+
+        foreach (int usernumber in usernumbers)
+        {
+            if(usernumber > largest)
+            {
+                largest = usernumber;
+            }
+        }
+
+        Console.WriteLine($"The largest number is: {largest}");
+
+
     }
 }
