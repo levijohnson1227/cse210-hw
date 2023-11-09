12b3b959be2507d8861c7849c17dc9c4b7c12bc2
levijohnson1227 <lw.johnson.1227@gmail.com>
2023-11-08 20:29:35 -0700

    learning05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("Blue",10);
+        // Console.WriteLine(square.GetColor());
+        // Console.WriteLine(square.GetArea());
+        shapes.Add(square);
+
+        Rectangle rectangle = new Rectangle("Red", 10, 15);
+        // Console.WriteLine(rectangle.GetColor());
+        // Console.WriteLine(rectangle.GetArea());
+        shapes.Add(rectangle);
+
+        Circle circle = new Circle("Indigo", 10);
+        // Console.WriteLine(circle.GetColor());
+        // Console.WriteLine(circle.GetArea());
+        shapes.Add(circle);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"Shape color: {color}, Shape Area: {area}");
+        }
     }
 }
